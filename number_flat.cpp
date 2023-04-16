93b4c9292afb62f7756e0516253e124de976fbb7
Ruslan Izmaylov <magnussmile@rambler.ru>
2023-04-16 14:53:29 +0300

    my next sol.

--- number_flat.cpp
+++ number_flat.cpp
@@ -24,10 +24,23 @@ void FlatNumber() {
     cout << 0 << ' ' << 0 << '\n';
     return;
   }
-  int flats_in_floor = flat2 % cnt_floors_to_2 == 0 ? flat2 / cnt_floors_to_2 : flat2 / cnt_floors_to_2 + 1;
-  int cnt_floors_to_1 = flat1 % flats_in_floor == 0 ? flat1 / flats_in_floor : flat1 / flats_in_floor + 1;
-  entrance1 = cnt_floors_to_1 % all_floors == 0 ? cnt_floors_to_1 / all_floors : cnt_floors_to_1 / all_floors + 1;
-  floor1 = cnt_floors_to_1 <= all_floors ? cnt_floors_to_1 : cnt_floors_to_1 % all_floors;
+  int max_flats_in_floor = flat2 % cnt_floors_to_2 == 0 ? flat2 / cnt_floors_to_2 : flat2 / cnt_floors_to_2 + 1;
+  int min_flats_in_floor = flat2 % (cnt_floors_to_2 - 1) == 0 ? flat2 / (cnt_floors_to_2 - 1) - 1 : flat2 / (cnt_floors_to_2 - 1);
+  int min_cnt_floors_to_1 = flat1 % max_flats_in_floor == 0 ? flat1 / max_flats_in_floor : flat1 / max_flats_in_floor + 1;
+  int max_cnt_floors_to_1 = flat1 % min_flats_in_floor == 0 ? flat1 / min_flats_in_floor : flat1 / min_flats_in_floor + 1;
+  if (min_cnt_floors_to_1 != max_cnt_floors_to_1) {
+    int max_entrance1 = max_cnt_floors_to_1 % all_floors == 0 ? max_cnt_floors_to_1 / all_floors : max_cnt_floors_to_1 / all_floors + 1;
+    int min_entrance1 = min_cnt_floors_to_1 % all_floors == 0 ? min_cnt_floors_to_1 / all_floors : min_cnt_floors_to_1 / all_floors + 1;
+    if (max_entrance1 != min_entrance1) {
+      cout << 0 << ' ' << 0 << '\n';
+      return;
+    } else {
+      cout << max_entrance1 << ' ' << 0 << '\n';
+      return;
+    }
+  }
+  entrance1 = max_cnt_floors_to_1 % all_floors == 0 ? max_cnt_floors_to_1 / all_floors : max_cnt_floors_to_1 / all_floors + 1;
+  floor1 = max_cnt_floors_to_1 <= all_floors ? max_cnt_floors_to_1 : max_cnt_floors_to_1 % all_floors;
   cout << entrance1 << ' ' << floor1 << '\n';
 }
 
