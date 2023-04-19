eb9d35ec89b2c9600e00d25c0f484beadb277e31
Ruslan Izmaylov <magnussmile@rambler.ru>
2023-04-19 10:15:35 +0300

    some try task J

--- Task_J_linear_equations_2.cpp
+++ Task_J_linear_equations_2.cpp
@@ -97,13 +97,94 @@ void LinearEquation() {
       return;
     }
     if (k1 == k2 && m1 == m2) {
-      cout << 1 << k1 << ' ' << m1 << '\n';
+      cout << 1 << ' ' << k1 << ' ' << m1 << '\n';
       return;
     }
   }
 
 }
+
+void LinearEquation_v2() {
+  cout.setf(ios::fixed);
+  cout.precision(6);
+  //ax + by = e,
+  //cx + dy = f
+  double a, b, c, d, e, f;
+  cin >> a >> b >> c >> d >> e >> f;
+
+  double determinant = a * d - b * c;
+  if (determinant == 0) {
+    if (a == 0 && c == 0) {
+      if (b == 0 && d == 0) {
+        if (e == 0 && f == 0) {
+          cout << 5 << endl;
+          return;
+        }
+        cout << 0 << endl;
+        return;
+      }
+      if (e * d != f * b) {
+        cout << 0 << endl;
+        return;
+      } else {
+        if (b != 0) {
+          cout << 4 << ' ' << e / b << endl;
+          return;
+        } else {
+          cout << 4 << ' ' << f / d << endl;
+          return;
+        }
+      }
+    }
+    if (b == 0 && d == 0) {
+      if (e * c != f * a) {
+        cout << 0 << endl;
+        return;
+      } else {
+        if (a != 0) {
+          cout << 3 << ' ' << e / a << endl;
+          return;
+        } else {
+          cout << 3 << ' ' << f / c << endl;
+          return;
+        }
+      }
+    }
+    if (a != 0) {
+      double coeff = c / a;
+      c = 0;
+      d -= coeff * b;
+      f -= coeff * e;
+      if (c == 0 && d == 0 && f == 0) {
+        cout << 1 << ' ' << -a / b << ' ' << e / b << endl;
+        return;
+      }
+    } else {
+      double coeff = a / c;
+      a = 0;
+      b -= coeff * d;
+      e -= coeff * f;
+      if (a == 0 && b == 0 && e == 0) {
+        cout << 1 << ' ' << -c / d << ' ' << f / d << endl;
+        return;
+      }
+    }
+  } else {
+    if (d != 0) {
+      double x = (e - b * f / d) / (a - b * c / d);
+      double y = (f - c * x) / d;
+      cout << 2 << ' ' << x << ' ' << y << endl;
+      return;
+    } else {
+      double x = (f - d * e / b) / (c - d * a / b);
+      double y = (e - a * x) / b;
+      cout << 2 << ' ' << x << ' ' << y << endl;
+      return;
+    }
+  }
+}
+
 int main() {
-  LinearEquation();
+  LinearEquation_v2();
   return 0;
 }
