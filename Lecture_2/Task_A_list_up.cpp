3e698adf90b155486b4f59c7de40b94b57e21a9b
Ruslan Izmaylov <magnussmile@rambler.ru>
2023-04-19 13:12:47 +0300

    Some pertrubation

---
+++ Lecture_2/Task_A_list_up.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+void UpList() {
+  string s;
+  getline(cin, s);
+  stringstream ss(s);
+  int first;
+  int x;
+  ss >> first;
+  bool flag = true;
+  while (ss >> x) {
+    if (x > first) {
+      first = x;
+    } else {
+      flag = false;
+      break;
+    }
+  }
+  if (flag) cout << "YES" << endl;
+  else cout << "NO" << endl;
+}
+
+int main()
+{
+  UpList();
+  return 0;
+}
---
+++ Lecture_2/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main_m()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
