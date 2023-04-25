9b5a4467fa1645231c3c1e4cbc3c12fc0c62e222
Ruslan Izmaylov <magnussmile@rambler.ru>
2023-04-25 11:33:19 +0300

    Another try Task G

--- Lecture_2/Task_G_bigger_multiplication.cpp
+++ Lecture_2/Task_G_bigger_multiplication.cpp
@@ -14,6 +14,38 @@ void ReadAndWriteSeq(vector<int>& nums) {
   }
 }
 
+pair<int, int> BiggerMult2() {
+  vector<int> nums;
+  ReadAndWriteSeq(nums);
+
+  if (nums.size() == 2) {
+    return nums[0] < nums[1] ? make_pair(nums[0], nums[1]) : make_pair(nums[1], nums[0]);
+  }
+  int first_pos_max = 0, second_pos_max = 0, first_neg_max = 0, second_neg_max = 0;
+
+  for (size_t i = 0; i < nums.size(); ++i) {
+    if (nums[i] > 0) {
+      if (nums[i] >= first_pos_max) {
+        second_pos_max = first_pos_max;
+        first_pos_max = nums[i];
+      }
+    } else {
+      if (nums[i] <= first_neg_max) {
+        second_neg_max = first_neg_max;
+        first_neg_max = nums[i];
+      }
+    }
+  }
+  int pos_prod = first_pos_max * second_pos_max;
+  int neg_prod = first_neg_max * second_neg_max;
+  if (pos_prod > neg_prod) {
+    return first_pos_max < second_pos_max ? make_pair(first_pos_max, second_pos_max) : make_pair(second_pos_max, first_pos_max);
+  } else {
+
+    return first_neg_max < second_neg_max ? make_pair(first_neg_max, second_neg_max) : make_pair(second_neg_max, first_neg_max);
+  }
+}
+
 pair<int, int> BiggerMult() {
   vector<int> nums;
   ReadAndWriteSeq(nums);
@@ -34,7 +66,7 @@ pair<int, int> BiggerMult() {
 }
 
 int main() {
-  pair<int, int> res = BiggerMult();
+  pair<int, int> res = BiggerMult2();
   cout << res.first << ' ' << res.second << endl;
   return 0;
 }
