00c3c3996dbb137a12eb6e431bba284dde88c3a3
Ruslan Izmaylov <izya.man18@gmail.com>
2023-05-04 16:20:16 +0300

    Second try Task_G

--- Lecture_3/Task_G_Turtles.cpp
+++ Lecture_3/Task_G_Turtles.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <set>
 
 using namespace std;
 
@@ -7,11 +8,20 @@ int Turtles()
   int N;
   int res = 0;
   cin >> N;
+  set<int> behind;
+  set<int> in_front;
   for (int i = 0; i < N; ++i)
   {
     int a, b;
     cin >> a >> b;
-    if (a + b == N - 1) ++res;
+    if (a + b == N - 1 &&
+        behind.find(a) == behind.end() &&
+        in_front.find(b) == in_front.end())
+    {
+      ++res;
+      behind.insert(a);
+      in_front.insert(b);
+    }
   }
   return res;
 }
